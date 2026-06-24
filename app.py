94de0b48e1edd94de8b744735929e65701995171
Anshika singh <anshika0713singh@gmail.com>
2026-06-24 12:06:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def speak():
     }
     body = {
         "text": text,
-        "model_id": "eleven_monolingual_v1",
+        "model_id": "eleven_turbo_v2_5",
         "voice_settings": {"stability": 0.5, "similarity_boost": 0.75}
     }
     response = req.post(url, headers=headers, json=body)
